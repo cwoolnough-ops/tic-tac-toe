d31fb778edfc77e83726410baba306a597f9ced4
cwoolnough-ops <cwoolnoughh@gmail.com>
2020-05-08 16:12:37 -0600

    added comments

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,5 @@
 import game_functions
-def game():
+def two_player_game():
     print("Lets play tic tac toe")
     #assigns player names
     game_functions.player_1[1] = input("\nwrite the name of player 1 \n")
@@ -11,29 +11,32 @@ def game():
     print("the numbers reffer to the key you must enter to mark that space on the board")
     # unused variable, used as a gate to start the game
     start = input("press any key to start the game! \n")
+    last_move = game_functions.player_2 # keeps track of the last player to make a move
+    filled_spots = [] #holds spots that have been filled already
+    valid_choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] 
     # continualy asks for player moves untill an ending condiition is met ie: is_game_over
-    last_move = game_functions.player_2
-    filled_spots = []
-    valid_choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     while game_functions.is_game_over(game_functions.board_dict) == 0:
+        # if else statement used to keep turn order
         if last_move == game_functions.player_2:
             current_player = game_functions.player_1
         else:
             current_player = game_functions.player_2
-        last_move = current_player
+        last_move = current_player # sets the last player to the player about to make a move
         game_functions.show_location_names()
         game_functions.current_board()
+        # choice is the location where the player wants to play
         choice = input(current_player[1] + "(" + current_player[0] + ") it is your turn where would you like to play? \npress the number where you want to play then the \"enter\" key \n")
+        # continually asks for input untill a valid choice is made
         while choice in filled_spots or choice not in valid_choices:
             if choice in filled_spots:
                 choice = input("that space is filled please try again\n")
             elif choice not in valid_choices:
                 choice = input("please chose a valid space\n")
         game_functions.move(choice, current_player)
-        # last_move = current_player
         filled_spots += choice
+    # prints the outcome of the game
     if game_functions.is_game_over(game_functions.board_dict) == 1:
         print("\n" + last_move[1] + " Wins!")
     else:
         print("\nThe game ends with a tie")
-game()
+two_player_game()
